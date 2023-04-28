d33c994688201d7dc45ed8679fa83127314792bf
Marcos Capistrano <mcapistrano50@gmail.com>
2023-04-28 15:58:44 -0300

    first

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,26 @@
  * for Docker builds.
  */
 await import("./src/env.mjs");
+import mdx from '@next/mdx'
 
-/** @type {import("next").NextConfig} */
-const config = {
+const withMDX = mdx({
+  extension: /\.mdx?$/,
+  options: {
+    // If you use remark-gfm, you'll need to use next.config.mjs
+    // as the package is ESM only
+    // https://github.com/remarkjs/remark-gfm#install
+    remarkPlugins: [],
+    rehypePlugins: [],
+    // If you use `MDXProvider`, uncomment the following line.
+    // providerImportSource: "@mdx-js/react",
+  },
+})
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Configure pageExtensions to include md and mdx
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+  // Optionally, add any other Next.js config below
   reactStrictMode: true,
 
   /**
@@ -18,5 +35,6 @@ const config = {
     locales: ["en"],
     defaultLocale: "en",
   },
-};
-export default config;
+}
+
+export default withMDX(nextConfig);
