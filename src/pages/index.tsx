48d2089775028de7c882caa7b0a6a266ed0ff408
Marcos Capistrano <mcapistrano50@gmail.com>
2023-04-28 16:46:49 -0300

    oi

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,8 +6,9 @@ import Link from "next/link";
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
   const user = useUser();
+
+  const {data} = api.posts.getAll.useQuery();
 
   return (
     <>
@@ -19,6 +20,9 @@ const Home: NextPage = () => {
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         {user.isSignedIn ? <SignOutButton /> : <SignInButton />}
         <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
+        {data?.map(post => (
+          <div>{post.content}</div>
+        ))}
       </main>
     </>
   );
